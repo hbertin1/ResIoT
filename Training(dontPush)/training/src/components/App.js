b25dfa339775ff137a7c2513dfea07f70765dcab
hugo <hugo.bertin35@gmail.com>
2022-03-07 18:14:09 +0100

    debut App react

---
+++ Training(dontPush)/training/src/components/App.js
@@ -0,0 +1,13 @@
+import Banner from './Banner'
+import Body from './Body'
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Body />
+    </div>
+  )
+}
+
+export default App;
---
+++ Training(dontPush)/training/src/components/Banner.js
@@ -0,0 +1,14 @@
+import '../styles/Banner.css'
+import logo_esir from '../assets/logo_esir.png'
+
+function Banner() {
+    const title = "le knx c'est super"
+    return (
+        <div className='knx-banner'>
+            <img src={logo_esir} alt='Logo ESIR' className='knx-logo' />
+            <h1 knx-title>{title}</h1>
+        </div>
+    )
+}
+
+export default Banner
