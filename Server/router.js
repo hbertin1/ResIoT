bb68ac80c9f52cfd5f673b8fa992080d029ea2f6
adbodin <adrien.bodin@etudiant.univ-rennes1.fr>
2022-03-22 16:24:56 +0100

    Récupération

--- Server/router.js
+++ Server/router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ReadableStreamBYOBRequest } = require("stream/web");
 const router = express.Router();
 const urlServer = '127.0.0.1:8080'
 
@@ -17,6 +18,42 @@ router
     .get('/chenillard', (request, response) => {
         if(request.query.signal === 'on')
             console.log("On")
-        else
-            console.log("off")
+            response.send("Chenillard")
+    })
+    .get('/led', (request, response) => {
+        if(request.query.signal === 'on'){
+            switch (request.query.signal){
+                case '1':
+                    console.log("led1 ON")
+                    break;
+                case '2':
+                    console.log("led2 ON")
+                    break;
+                case '3':
+                    console.log("led3 ON")
+                    break;
+                case '4':
+                    console.log("led4 ON")
+                    break;
+            }
+        }
+        else if(request.query.signal === 'off'){
+            switch (request.query.signal){
+                case '1':
+                    console.log("led1 Off")
+                    break;
+                case '2':
+                    console.log("led2 Off")
+                    break;
+                case '3':
+                    console.log("led3 Off")
+                    break;
+                case '4':
+                    console.log("led4 Off")
+                    break;
+                case 'All':
+                    console.log("All led ")
+                    break;
+            }
+        }
     })
