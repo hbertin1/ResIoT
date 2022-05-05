aec0faebeca8ff4559775d2e4657224139e28d39
adbodin <adrien.bodin@etudiant.univ-rennes1.fr>
2022-05-05 10:40:23 +0200

    Réparation des conflits de version

--- Client/src/components/Display.js
+++ Client/src/components/Display.js
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux'
 
 const axios = require('axios')
 const urlServer = '127.0.0.1:8000'
-const client = new W3CWebSocket('ws://127.0.0.1:3000');
 
 
 function Display({ id }) {
--- Client/src/components/parserServer.js
+++ Client/src/components/parserServer.js
@@ -1,17 +1,8 @@
-import {createStore} from 'redux';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-const client = new W3CWebSocket('ws://127.0.0.1:3000');
+import { createStore } from "redux";
 
-client.onopen = () => {
-    console.log('WebSocket Client Connected');
-    client.send(JSON.stringify({"connexion":true}))
-};
-client.onmessage = (message) => {
-    console.log(message);
-    let dataReceived = JSON.parse(message.data)
-    console.log(dataReceived.knx)
-    dispatch({
-        "type": "setLedNumber",
-        "number": 5
-      })    
-};
+// {"knx" : this.knx, "numberLed": this.numberLed, "chenillard":this.chenillard}
+
+export function parseDataRcvd(data) {
+    let dataRcvd = JSON.parse(data)
+console.log(dataRcvd)
+} 
