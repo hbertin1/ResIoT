55bae7d3fe765f91553492f0e29b456b7b02cb62
hugo <hugo.bertin35@gmail.com>
2022-05-20 10:17:26 +0200

    debut Slider

--- Server/knx_monitor.js
+++ Server/knx_monitor.js
@@ -6,7 +6,7 @@ const json = new Json();
 
 var connection = new knx.Connection({
   // ip address and port of the KNX router or interface
-  ipAddr: '192.168.0.201', ipPort: 3671,
+  ipAddr: '192.168.0.202', ipPort: 3671,
   // in case you need to specify the multicast interface (say if you have more than one)
   // interface: 'utun2',
   // the KNX physical address we'd like to use
